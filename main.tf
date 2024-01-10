927352dba67148d95a00e4226052e7c4514a7004
DevOpsMaster76 <practicedevops56@gmail.com>
2024-01-10 08:50:53 +0530

    yesterday notes

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-  source = "../terraform-aws-vpc"
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
@@ -10,5 +11,11 @@ module "roboshop" {
 
   # private subnet
   private_subnets_cidr = var.private_subnets_cidr
+
+  # database subnet
+  database_subnets_cidr = var.database_subnets_cidr
+
+  #peering
+  is_peering_required = var.is_peering_required
 
 }
